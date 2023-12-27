a1112cc3c3b6dd9c8bf1f69f8070b24a0ba6df0a
dpogudina <darynet.com@gmail.com>
2023-12-27 11:26:31 +0400

    fix urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 
 urlpatterns = [
@@ -6,3 +8,8 @@ urlpatterns = [
     path("api/", include("users.urls", namespace="api_users")),
     path("api/", include("api.urls", namespace="api")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
